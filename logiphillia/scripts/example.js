e482bc53ad9fbb9bcea81e0071a93775777902a7
Vismai Nair <vismai.nair@gmail.com>
2024-01-19 13:30:05 -0700

    Add files via upload

---
+++ logiphillia/scripts/example.js
@@ -0,0 +1,10 @@
+function changeToCSS(){
+    document.getElementById("what").innerHTML = "Now, we've added some CSS!"
+    document.getElementById("description").innerHTML = "It's not so boring now, is it?"
+    document.getElementById("html").style.backgroundColor = "#1da58a";
+    document.getElementById("html").style.color = "white";
+    document.getElementById("html").style.fontFamily = "sans-serif";
+    document.getElemetById("button").style.borderRadius = "50px";
+    document.getElementById("button").style.backgroundColor = "#13025";
+    
+  }
