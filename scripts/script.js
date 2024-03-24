61211a48d05b382ee50d34d1d2ce4fe191f051b3
Vismai Nair <vismai.nair@gmail.com>
2024-03-23 17:34:28 -0700

    5.0 Add _headers file

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,13 @@ $$('h1').forEach(function(h1){
     s.width = len + 'ch';
     s.animationTimingFunction = "steps('+len+'),steps(1)"
 });
+function changeToCSS(){
+    document.getElementById("what").innerHTML = "Now, we've added some CSS!"
+    document.getElementById("description").innerHTML = "It's not so boring now, is it?"
+    document.getElementById("html").style.backgroundColor = "#1da58a";
+    document.getElementById("html").style.color = "white";
+    document.getElementById("html").style.fontFamily = "sans-serif";
+    document.getElemetById("button").style.borderRadius = "50px";
+    document.getElementById("button").style.backgroundColor = "#13025";
+    
+  }
